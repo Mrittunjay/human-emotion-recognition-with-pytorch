a2cda5b3383ca47811112977aade275565c52d49
Sipan <paul.mrittunjoy777@gmail.com>
2024-07-13 09:20:09 +0530

    Added main block for model training and inference

--- emotion-recognition.py
+++ emotion-recognition.py
@@ -6,6 +6,10 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm.auto import tqdm
+from timeit import default_timer as timer
 
 
 DATA_PATH = Path("data")
@@ -132,18 +136,18 @@ print(f"Train data image shape: {img.shape}")
 print(f"image dtype: {img.dtype}")
 print(f"Label datatype {label}: {type(label)}")
 
-# Rearranging tensor object 'img' to display image using matplotlib
-img_permuted = img.permute(1, 2, 0)
-# plotting the image
-plt.figure(figsize=(8, 6))
-plt.imshow(img_permuted)
-plt.axis('off')
-plt.title(idx_to_class[label], fontsize=14)
-plt.show()
+# # Rearranging tensor object 'img' to display image using matplotlib
+# img_permuted = img.permute(1, 2, 0)
+# # plotting the image
+# plt.figure(figsize=(8, 6))
+# plt.imshow(img_permuted)
+# plt.axis('off')
+# plt.title(idx_to_class[label], fontsize=14)
+# plt.show()
 
 
 # Creating my dataloaders
-BATCH_SIZE=5
+BATCH_SIZE=128
 train_dataloader = DataLoader(dataset=train_data,
                               batch_size=BATCH_SIZE,
                               num_workers=os.cpu_count(),
@@ -155,9 +159,66 @@ test_dataloader = DataLoader(dataset=test_data,
 
 
 # Creating model architecture here:
-class custom_emotion_recognition(torch.nn.Module):
+class Custom_Emotion_Recognition(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(Custom_Emotion_Recognition, self).__init__()
+        self.conv1 = nn.Conv2d(1,10,3)
+        self.conv2 = nn.Conv2d(10,10,3)
+        self.pool2 = nn.MaxPool2d(2,2)
+
+        self.conv3 = nn.Conv2d(10,10,3)
+        self.conv4 = nn.Conv2d(10,10,3)
+        self.pool4 = nn.MaxPool2d(2,2)
+
+        self.norm = nn.BatchNorm2d(10)
+
+        self.fc1 = nn.Linear(810,50)
+        self.fc2 = nn.Linear(50,3)      # 3 for three output classes
+
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+
+        self.fc_loc = nn.Sequential(
+            nn.Linear(640, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3*2)
+        )
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1,0,0,0,1,0], dtype=torch.float))
+
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 640)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+        return x
+
+    def forward(self, input):
+        out = self.stn(input)
+
+        out = F.relu(self.conv1(out))
+        out = self.conv2(out)
+        out = F.relu(self.pool2(out))
+
+        out = F.relu(self.conv3(out))
+        out = self.norm(self.conv4(out))
+        out = F.relu(self.pool4(out))
+
+        out = F.dropout(out)
+        out = out.view(-1, 810)
+        out = F.relu(self.fc1(out))
+        out = self.fc2(out)
+
+        return out
+
 
 
 # Creating train step function for the training loop
@@ -204,7 +265,7 @@ def train_step(model: torch.nn.Module,
 
 # Creating a test step function for training loop
 def test_step(model: torch.nn.Module,
-              dataloader: torch.unils.data.DataLoader,
+              dataloader: torch.utils.data.DataLoader,
               loss_fn: torch.nn.Module,
               device=device):
     # Put model into evaluation mode
@@ -235,3 +296,82 @@ def test_step(model: torch.nn.Module,
         test_loss = test_loss/len(dataloader)
         test_acc = test_acc/len(dataloader)
         return test_loss, test_acc
+
+# Train function:
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          device: str = 'cpu',
+          epochs: int = 5):
+    # Creating empty result dictionary
+    results = {"train_loss": [],
+               "train_acc": [],
+               "val_loss": [],
+               "val_acc": []}
+
+    # Looping through training and testing step
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(
+            model=model,
+            dataloader=train_dataloader,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            device=device
+        )
+        test_loss, test_acc = test_step(
+            model=model,
+            dataloader=test_dataloader,
+            loss_fn=loss_fn,
+            device=device
+        )
+
+        # Print details
+        print(f"\nEpoch: {epoch} | Train loss: {train_loss:0.4f} | Train acc: {train_acc*100:.2f}% | test loss: {test_loss:.4f} | test_acc: {test_acc*100:.2f}%")
+
+        results['train_loss'].append(train_loss)
+        results['train_acc'].append(train_acc)
+        results['test_loss'].append(test_loss)
+        results['test_acc'].append(test_acc)
+        return results
+
+
+if __name__ == '__main__':
+    NUM_EPOCHS = 100
+
+    # Creating my dataloaders
+    BATCH_SIZE = 128
+    train_dataloader = DataLoader(dataset=train_data,
+                                  batch_size=BATCH_SIZE,
+                                  num_workers=os.cpu_count(),
+                                  shuffle=True)
+    test_dataloader = DataLoader(dataset=test_data,
+                                 batch_size=BATCH_SIZE,
+                                 num_workers=os.cpu_count(),
+                                 shuffle=False)
+
+    # Creating model instance
+    emotion_model_v1 = Custom_Emotion_Recognition()
+
+    # Training Loop:
+    # Setting loss function and optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(params=emotion_model_v1.parameters(),
+                                 lr=0.005)
+
+    # Training start time
+    start_time = timer()
+
+    # Run training function
+    emotion_model_v1_results = train(model=emotion_model_v1,
+                                     train_dataloader=train_dataloader,
+                                     test_dataloader=test_dataloader,
+                                     optimizer=optimizer,
+                                     loss_fn=loss_fn,
+                                     epochs=NUM_EPOCHS,
+                                     device=device)
+
+    # Training end time
+    end_time = timer()
+    print(f"Total training time: {((end_time - start_time) / 60) / 60} hrs")
